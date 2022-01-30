a40851fc931c0d6ad99c7ff97c17e9c9071693ea
Dennis Akagha <d.akagha20@gmail.com>
2022-01-31 00:22:32 +0100

    Make some changes

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const secsEl = document.querySelector('.secs');
 
 setInterval(() => {
   const date = new Date();
-  const secsDegree = (date.getSeconds() / 60) * (360 - 90);
-  const minsDegree = (date.getMinutes() / 60) * (360 - 90);
-  const hrsDegree = (date.getHours() / 12) * (360 - 90);
+  const secsDegree = (date.getSeconds() / 60) * 360 - 90;
+  const minsDegree = (date.getMinutes() / 60) * 360 - 90;
+  const hrsDegree = (date.getHours() / 12) * 360 - 90;
   secsEl.style.transform = `rotate(${secsDegree}deg)`;
   minsEl.style.transform = `rotate(${minsDegree}deg)`;
   hourEl.style.transform = `rotate(${hrsDegree}deg)`;
