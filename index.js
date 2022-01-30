287d20a7f39bdd25e464171f49782f7c5bc2bcff
Dennis Akagha <d.akagha20@gmail.com>
2022-01-30 21:06:51 +0100

    Integrate functionality for the analogu clock

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const hourEl = document.querySelector('.hour');
 const minsEl = document.querySelector('.mins');
 const secsEl = document.querySelector('.secs');
 
-
 setInterval(() => {
-    const date = new Date;
-    const secsDegree = date.getSeconds()/60 * 360 - 90;
-    const minsDegree = date.getMinutes() /60 * 360 -90;
-    const hrsDegree = date.getHours() / 12 * 360 -90;
-    secsEl.style.transform = `rotate(${secsDegree}deg)`
-    minsEl.style.transform = `rotate(${minsDegree}deg)`
-    hourEl.style.transform = `rotate(${hrsDegree}deg)`
+  const date = new Date();
+  const secsDegree = date.getSeconds() / 60 * 360 - 90;
+  const minsDegree = date.getMinutes() / 60 * 360 - 90;
+  const hrsDegree = date.getHours() / 12 * 360 - 90;
+  secsEl.style.transform = `rotate(${secsDegree}deg)`;
+  minsEl.style.transform = `rotate(${minsDegree}deg)`;
+  hourEl.style.transform = `rotate(${hrsDegree}deg)`;
 }, 1000);
